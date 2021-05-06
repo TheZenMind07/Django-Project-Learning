40a934e9f8d3afe88ec59d7b8dadee3e1800b596
Rajkishor Pradhan <rajp7598@gmail.com>
2021-05-06 17:08:37 +0530

    testing contect in templating

--- src/newdjango/newdjango/urls.py
+++ src/newdjango/newdjango/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from pages.views import home_view
 from pages.views import contact_view
+from pages.views import about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
-    path('contact/', contact_view, name="Contact" )
+    path('contact/', contact_view, name="contact" ),
+    path('about/', about_view, name="about" )
+
 ]
--- src/newdjango/pages/views.py
+++ src/newdjango/pages/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_view(request,*args, **kwargs):
-        print(request.user)
-        print(args, kwargs)
-        return HttpResponse("<h1> Hello Django web </h1>")
+        # print(request.user)
+        # print(args, kwargs)
+        # return HttpResponse("<h1> Hello Django web </h1>")
+        return render(request, "index.html", {})
 
 
-def contact_view(*args, **kwargs):
-        return HttpResponse("<h1>COntact Of the dJangao</h1>")
+def contact_view(request,*args, **kwargs):
+        my_context = {
+                "key1" : "Item1",
+                "key2" : "item2",
+                "arr" : [123,4851,21581,"dfg"]
+        }
+        return render(request, "contact.html", my_context)
+
+def about_view(request,*args, **kwargs):
+        return render(request, "about.html", {})
